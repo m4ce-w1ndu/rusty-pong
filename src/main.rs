4e5b6947ddd3bd51edda95d095da8d6fd4016388
m4ce-w1ndu <m4ce.w1ndu@icloud.com>
2024-04-10 02:00:36 +0200

    Added application properties

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::{
+    prelude::*,
+    window::{PresentMode, WindowTheme}
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Rusty Pong".into(),
+                name: Some("rusty.pong".into()),
+                resolution: (828.0, 525.0).into(),
+                present_mode: PresentMode::AutoVsync,
+                window_theme: Some(WindowTheme::Dark),
+                ..default()
+            }), ..default()
+        }))
+        .run();
 }
