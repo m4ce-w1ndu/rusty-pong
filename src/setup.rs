5d5af16e7e08216e3ed567df61ac2b7bf36d3b1a
m4ce-w1ndu <m4ce.w1ndu@icloud.com>
2024-04-10 02:10:13 +0200

    Add documentation and constants to setup

--- src/setup.rs
+++ src/setup.rs
@@ -5,7 +5,17 @@ use bevy::{
     window::{WindowPlugin, Window, PresentMode, WindowTheme},
 };
 
+/// Configuration plugin.
+/// 
+/// This plugin sets up the application, rendering the first
+/// 2D shapes that will make the playing field for the game.
 pub struct AppConfiguration;
+
+/// Default Pong screen width
+pub const SCREEN_WIDTH: f32 =  828.0;
+
+/// Default Pong screen height
+pub const SCREEN_HEIGHT: f32 = 525.0;
 
 impl Plugin for AppConfiguration {
     fn build(&self, app: &mut bevy::prelude::App) {
@@ -13,7 +23,7 @@ impl Plugin for AppConfiguration {
             primary_window: Some(Window {
                 title: "Rusty Pong".into(),
                 name: Some("rusty.pong".into()),
-                resolution: (828.0, 525.0).into(),
+                resolution: (SCREEN_WIDTH, SCREEN_HEIGHT).into(),
                 present_mode: PresentMode::AutoVsync,
                 window_theme: Some(WindowTheme::Dark),
                 ..default()
