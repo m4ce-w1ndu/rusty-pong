28db99319fe957776024020eaae4dab0b373f638
m4ce-w1ndu <m4ce.w1ndu@icloud.com>
2024-04-15 18:25:25 +0200

    Added paddles to setup

--- src/setup.rs
+++ src/setup.rs
@@ -3,7 +3,24 @@ use bevy::{
     sprite::MaterialMesh2dBundle
 };
 
-use crate::components::BallBundle;
+use crate::components::{
+    BallBundle,
+    PaddleBundle
+};
+
+pub const INIT_PADDLE_X: f32 = 20.0;
+pub const INIT_PADDLE_Y: f32 = -25.0;
+
+pub struct SetupPlugin;
+
+impl Plugin for SetupPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, (
+            spawn_ball,
+            spawn_paddles
+        ));
+    }
+}
 
 /// Spawn the ball in the field
 pub fn spawn_ball(
@@ -24,6 +41,24 @@ pub fn spawn_ball(
         MaterialMesh2dBundle {
             mesh: mesh_handle.into(),
             material: material_handle,
+            ..default()
+        }
+    ));
+}
+
+pub fn spawn_paddles(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>
+) {
+    let mesh = Mesh::from(Rectangle::new(PaddleBundle::PADDLE_WIDTH, PaddleBundle::PADDLE_HEIGHT));
+    let material = ColorMaterial::from(Color::rgb(0.0, 1.0, 0.0));
+
+    commands.spawn((
+        PaddleBundle::new(INIT_PADDLE_X, INIT_PADDLE_Y),
+        MaterialMesh2dBundle {
+            mesh: meshes.add(mesh).into(),
+            material: materials.add(material),
             ..default()
         }
     ));
